88d419afe18b9d56e2c31ce1d66fbc6e76aab90d
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-11-12 14:59:05 -0500

    User class Model added

--- src/main/java/com/snipper/snippets/model/User.java
+++ src/main/java/com/snipper/snippets/model/User.java
@@ -1,4 +1,46 @@
 package com.snipper.snippets.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String language;
+    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", language='" + language + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
 }
